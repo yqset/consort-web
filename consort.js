167c573ece28b52a9a30017cc0b61394d610b70f
Ying Qing Set <yqset@attu2.cs.washington.edu>
2014-11-12 10:48:50 -0800

    Ready for simple testing

--- consort.js
+++ consort.js
@@ -3,4 +3,26 @@ window.onload = function () {
 	document.getElementById("login").onclick = userLogin;
 	document.getElementById("session").onclick = userJoinSession;
 	document.getElementById("update").onclick = userUpdate;
+
+	function userLogin() {
+		alert(this.value);
+	}
+
+	function userJoinSession() {
+		alert(this.value);
+	}
+
+	function userUpdate() {
+		var ajax = new XMLHttpRequest();
+		ajax.onload = returnUpdate;
+		ajax.open("POST", "Test", true);
+		ajax.send("name=yqset&sesison=99");
+	}
+
+	function returnUpdate() {
+		var jsonVal = JSON.parse(this.responseText);
+		var preBlock = document.createElement("pre");
+		preBlock.innerHTML = jsonVal;
+		document.getElementById("jsonArea").append(preBlock); 
+	}
 }
