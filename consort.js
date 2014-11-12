9a74198d2d4b40785648ed8b0f86afea0d90a739
Ying Qing Set <yqset@attu2.cs.washington.edu>
2014-11-11 23:32:54 -0800

    added button onclicks

--- consort.js
+++ consort.js
@@ -1,3 +1,6 @@
 window.onload = function () {
 	alert("Check");
+	document.getElementById("login").onclick = userLogin;
+	document.getElementById("session").onclick = userJoinSession;
+	document.getElementById("update").onclick = userUpdate;
 }
